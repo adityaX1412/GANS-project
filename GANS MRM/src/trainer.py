189d26665714c8e67f1348a2c96fbc3e4746d805
adityaX1412 <148233635+adityaX1412@users.noreply.github.com>
2024-08-15 01:03:41 +0530

    Update trainer.py

--- GANS MRM/src/trainer.py
+++ GANS MRM/src/trainer.py
@@ -6,6 +6,20 @@ from torch.autograd import Variable
 # Initialize wandb
 wandb.init(project="DCGAN-CelebA-Inpainting", config=opt.__dict__)
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
+parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
+parser.add_argument("--lr", type=float, default=0.0001, help="adam: learning rate")
+parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
+parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
+parser.add_argument("--n_cpu", type=int, default=4, help="number of cpu threads to use during batch generation")
+parser.add_argument("--latent_dim", type=int, default=100, help="dimensionality of the latent space")
+parser.add_argument("--img_size", type=int, default=64, help="size of each image dimension")
+parser.add_argument("--channels", type=int, default=3, help="number of image channels")
+parser.add_argument("--sample_interval", type=int, default=400, help="interval between image sampling")
+parser.add_argument("--wd", type=float, default=0.003, help="weight decay for the optimizer")
+opt = parser.parse_args(args=[])
+
 # Training loop
 for epoch in range(opt.n_epochs):
     for i, (masked_imgs, real_imgs) in enumerate(dataloader):
