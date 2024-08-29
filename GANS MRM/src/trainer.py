8614752bcec1b6a5b59e0c71c6d0141529f73c17
adityaX1412 <148233635+adityaX1412@users.noreply.github.com>
2024-08-29 18:31:13 +0530

    Update trainer.py

--- GANS MRM/src/trainer.py
+++ GANS MRM/src/trainer.py
@@ -32,7 +32,7 @@ from utils.gans import Generator, Discriminator
 cuda = True if torch.cuda.is_available() else False
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--n_epochs", type=int, default=10, help="number of epochs of training")
+parser.add_argument("--n_epochs", type=int, default=100, help="number of epochs of training")
 parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
 parser.add_argument("--lr", type=float, default=0.0001, help="adam: learning rate")
 parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
